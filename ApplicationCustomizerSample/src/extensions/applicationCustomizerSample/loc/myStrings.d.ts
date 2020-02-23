11fa580875f629c2d446132b9c903939d45cbb04
Hiroaki Oikawa <herakles@orivers.jp>
2020-02-24 00:20:06 +0900

    new

---
+++ ApplicationCustomizerSample/src/extensions/applicationCustomizerSample/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IApplicationCustomizerSampleApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'ApplicationCustomizerSampleApplicationCustomizerStrings' {
+  const strings: IApplicationCustomizerSampleApplicationCustomizerStrings;
+  export = strings;
+}
